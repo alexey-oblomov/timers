9671404436d8a05482ef5322bd6c4df3ae66d2b5
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-03-26 09:43:19 +0300

    Добавил табы

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Tabs } from 'antd';
 import './App.css';
 
 function App() {
+  const { TabPane } = Tabs;
+
+  function callback(key) {
+    console.log(key);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +16,19 @@ function App() {
         </p>
       </header>
       <div className="App">
-        <Button type="primary">Button</Button>
+        <Tabs onChange={callback} type="card">
+          <TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+    </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+    </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+    </TabPane>
+        </Tabs>,
       </div>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
