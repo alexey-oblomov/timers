30760b4516f8c0a245789231676b6ba108e2e3db
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-03-28 19:33:08 +0300

    сделан компонент clock (часы)

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Button, Tabs } from 'antd';
+import { Tabs } from 'antd';
 import Timer from './components/Timer/Timer';
 import Countdown from './components/Countdown/Countdown';
+import Clock from './components/Clock/Clock';
 import './App.css';
 
 function App() {
   const { TabPane } = Tabs;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Здесь будут таймеры!</h1>
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Таймеры</h1>
       </header>
-      <div className="App">
+      <div className="app">
         <Tabs type="card">
-          <TabPane tab="Таймер" key="1">
-            <Timer />
-            <div className="button">
-              <Button type="primary">Будет какая-то кнопка</Button>
-            </div>
+          <TabPane tab="Часы" key="1">
+            <Clock />
           </TabPane>
-          <TabPane tab="Счетчик обратного времени" key="2">
+          <TabPane tab="Таймер" key="2">
+            <Timer />
+          </TabPane>
+          <TabPane tab="Счетчик обратного времени" key="3">
             <Countdown />
           </TabPane>
         </Tabs>
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
+import { Button } from 'antd';
+import './Timer.css';
+import TimerDisplay from './TimerDisplay';
 
-export default function Timer() {
-  return (
-    <div className="timer">
-      <h1>Таймер</h1>
-    </div>
-  );
+export default class Timer extends React.Component {
+  state = {
+    currentTime: new Date(),
+  };
+
+  render() {
+    return (
+      <div className="timer-container">
+        <TimerDisplay
+          currentTime={this.state.currentTime.toLocaleTimeString()}
+        />
+        <div className="btn-container">
+          <div className="btn">
+            <Button type="primary">Запустить/пауза</Button>
+          </div>
+          <div className="btn">
+            <Button type="primary">Сбросить</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
